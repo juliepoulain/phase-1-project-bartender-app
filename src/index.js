857f3465e84b5d336db03fe53d694cba7f98e786
Julie Poulain <juliempoulain@gmail.com>
2024-03-22 12:33:46 -0700

    add function templates to index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-import {API_KEY} from "/api-key/config.js";
-console.log(API_KEY)
+import { API_KEY } from "/api-key/config.js";
+console.log(API_KEY);
+
+const url = "www.thecocktaildb.com/api/json/v1/1/search.php?";
+
+//JP: fetches ingredients and populates select list with ingredient options
+const populateIngredients = (ingredient) => {};
+
+//JP: create submit event listener
+const createSubmitListener = () => {};
+
+//BD: fetches drinks and populates list with all for initial load
+const populateDrinksInitial = (drink) => {};
+
+//PA: handle submit to refetch and repopulate drink list with filtered data based on selected ingredients
+const handleSubmit = () => {};
 
 document.addEventListener("DOMContentLoaded", () => {
+  //full execution
+  main();
+});
 
-
-    //fetch variables 
-    const url = ""
-
-
-
-
-
-    // Event listners 
-
-
-
-
-    
-    // Execution 
-    const main = () => {
-
-    }
-
-})
+const main = () => {};
